b6f3fd289ddf756d54fd730af59e4fde2cd65019
Piyush Dubey <premdubey10981@gmail.com>
2025-10-23 23:07:55 +0530

    TimeLock

---
+++ contracts/TimeLock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/**
+ * @title TimeLock
+ * @dev A contract where users can deposit funds that are locked until a
+ * specified unlock time.
+ */
+contract TimeLock {
+    // State variables will be added here
+    
+    // Events will be added here
+
+    // Functions will be added here
+
+    constructor() {
+        // Constructor logic (if any)
+    }
+}
